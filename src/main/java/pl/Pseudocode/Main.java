24e04f4515e96c5e55e34d5e24d4889560a95f83
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-01-18 16:01:09 +0100

    sumOfPositiveNumbers added

---
+++ src/main/java/pl/Pseudocode/Main.java
@@ -0,0 +1,13 @@
+package pl.Pseudocode;
+
+public class Main {
+    public static void main(String[] args) {
+        PseudocodeExcersiseImpl pseudocodeExcersise = new PseudocodeExcersiseImpl();
+        int[] arrayOfNumbers = new int[] {2, 8, 46, 84, 236, 984, 1024, 45, -23};
+        int result = pseudocodeExcersise.highestValue(arrayOfNumbers);
+        System.out.println(result);
+        boolean secondResult = pseudocodeExcersise.isNumberEven(arrayOfNumbers);
+        System.out.println(secondResult);
+
+    }
+}
---
+++ src/main/java/pl/Pseudocode/PseudocodeExcersiseImpl.java
@@ -0,0 +1,38 @@
+package pl.Pseudocode;
+
+public class PseudocodeExcersiseImpl {
+
+
+    public int highestValue(int[] arrayOfNumbers) {
+        int highest = arrayOfNumbers[0];
+        for (int i : arrayOfNumbers) {
+            if (i > highest) {
+                highest = i;
+            }
+        }
+        return highest;
+
+    }
+
+
+    public boolean isNumberEven(int[] arrayOfNumbers) {
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] % 2 == 0) {
+                System.out.println("true");
+            }
+        }
+        return true;
+    }
+
+    public int sumOfPositiveNumbers(int[] arrayOfNumbers) {
+        int sum = 0;
+        int counter = 0;
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] >= 0) {
+                sum = sum + arrayOfNumbers[i];
+                counter++;
+            }
+        }
+        return (sum / counter);
+    }
+}
