91fcaf251db95699450156d2be2d40e7f8c28555
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-01-18 14:48:48 +0100

    Factorial added

--- src/main/java/pl/Recursion/AppGCD.java
+++ src/main/java/pl/Recursion/AppGCD.java
@@ -4,11 +4,13 @@ public class AppGCD {
     public static void main(String[] args) {
         int a = 3135;
         int b = 1257;
+        int c = 25;
         int greatestCommonDivisor = GreatestCommonDivisor.greatestCommonDivisor(a, b);
         System.out.println("Greatest common divisor for: " + a + " and " + b +  " is: " + greatestCommonDivisor);
         int gcdRecursion = GreatestCommonDivisor.gcdRecursion(a, b);
         System.out.println("Greatest common divisor for: " + a + " and " + b +  " is: " + gcdRecursion);
-
+        long factorial = Factorial.factorial(c);
+        System.out.println("Factorial for number " + c + " is: " + factorial);
 
     }
 }
---
+++ src/main/java/pl/Recursion/Factorial.java
@@ -0,0 +1,19 @@
+package pl.Recursion;
+
+public class Factorial {
+    public static long factorial(int c) {
+        if (c == 1) {
+            return 1;
+        }
+        return c * factorial(c - 1);
+    }
+
+}
+
+// public static long ex1(int a, int b) {
+//        long wynik = 1;
+//        for (int i = 0; i < b; i++) {
+//            wynik = a * wynik;
+//        }
+//        return wynik;
+//    }
