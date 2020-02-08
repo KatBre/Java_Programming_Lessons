add09818afd6dd224b919ce131d8695e5d5a83d3
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-02-08 10:42:27 +0100

    filter commit

--- src/main/java/pl/SixthDay15/AppSix.java
+++ src/main/java/pl/SixthDay15/AppSix.java
@@ -4,5 +4,10 @@ public class AppSix {
     public static void main(String[] args) {
         StreamsExamples.loopExample();
         StreamsExamples.streamExample();
+        WordProvider.provide();
+        WordProvider.streamWordsWithD(WordProvider.provide());
+        WordProvider.streamWordsWithAAndFourLetters(WordProvider.provide());
+        WordProvider.streamWordsWithAAndFourLetters(WordProvider.provide());
+        WordProvider.changeFirstLetter(WordProvider.provide());
     }
 }
